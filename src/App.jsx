092829752b9a81200a0c00dff8752bcaf875fa32
Evasaraf <164766438+Evasaraf@users.noreply.github.com>
2026-09-01 05:26:18 +0530

    creating a body.jsx to handle body components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./NavBar";
+import Body from "./Body";
 
 function App() {
   return (
     <>
     <BrowserRouter basename = "/">
      <Routes> 
-      <Route path = "/" element={<div> Base page</div>}/>
-      <Route path = "/login" element={<div> Login page</div>}/>
-      <Route path = "/test" element={<div> test page</div>}/>
+      <Route path = "/" element={Body}/>
      </Routes>
      </BrowserRouter>
 
      <  NavBar />
-      <h1 className="bg-red-500 text-white text-3xl font-bold p-10">
-        hello eva
-      </h1>
     </>
   );
 }
---
+++ src/Body.jsx
@@ -0,0 +1,9 @@
+import NavBar from "./NavBar";
+const Body = () =>{
+    return <div>
+            <NavBar /> 
+           </div>
+
+    
+}
+export default Body;
