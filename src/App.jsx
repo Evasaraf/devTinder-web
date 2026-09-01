764feb996012febaa2953d45b66f91b313f67c03
Evasaraf <164766438+Evasaraf@users.noreply.github.com>
2026-09-01 05:56:55 +0530

    creating a login router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "./NavBar";
 import Body from "./Body";
+import Login from "./Login";
 
 function App() {
   return (
-    <>
-    <BrowserRouter basename = "/">
-     <Routes> 
-      <Route path = "/" element={Body}/>
-     </Routes>
-     </BrowserRouter>
-
-     <  NavBar />
-    </>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Body />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/Body.jsx
+++ src/Body.jsx
@@ -1,9 +1,13 @@
+import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
-const Body = () =>{
-    return <div>
-            <NavBar /> 
-           </div>
 
-    
-}
+const Body = () => {
+  return (
+    <div>
+      <NavBar />
+      <Outlet />
+    </div>
+  );
+};
+
 export default Body;
