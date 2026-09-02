2df34ca30a89cd3bc322d1f711d45615d5170ceb
Evasaraf <164766438+Evasaraf@users.noreply.github.com>
2026-09-02 19:56:38 +0530

    updating login file

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,37 +1,51 @@
 import Footer from "./Footer";
-const Login = () => {
-  return (
-    <div> 
-     <h1>Login Page</h1>
-    <div className="flex justify-center my-10">
-    <div classname= "card bg-base-300 w-96 shadow-xl">
-    < div className="card-body">
-    <h2 className="card-title justify-center">Login</h2>
-      <div>   
-        <label className="form-control w-full max-w-xs">
-          <div className="label">
-          <span className="label-text">Email ID</span>
-       </div>
+import { useState } from "react";
 
-      <input  type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs"/>
-     </label>
-     <label className="form-control w-full max-w-xs py-5">
-          <div className="label">
-          <span className="label-text ">Password</span>
+const Login = () => {
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <div className="flex justify-center my-10">
+        <div className="card bg-base-300 w-96 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title justify-center">Login</h2>
+            <div>
+              <label className="form-control w-full max-w-xs">
+                <div className="label">
+                  <span className="label-text">Email ID</span>
+                </div>
+                <input
+                  type="text"
+                  value={emailId}
+                  placeholder="Type here"
+                  className="input input-bordered w-full max-w-xs"
+                  onChange={(e) => setEmailId(e.target.value)}
+                />
+              </label>
+              <label className="form-control w-full max-w-xs py-5">
+                <div className="label">
+                  <span className="label-text">Password</span>
+                </div>
+                <input
+                  type="password"
+                  value={password}
+                  placeholder="Type here"
+                  className="input input-bordered w-full max-w-xs"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </label>
+            </div>
+            <div className="card-actions justify-center">
+              <button className="btn btn-primary">Login</button>
+            </div>
           </div>
-        <input  type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs "/>
-     </label>
-     </div>
-    
-    <div className="card-actions justify-center">
-      <button className="btn btn-primary ">Login</button>
+        </div>
+      </div>
+      <Footer />
     </div>
-    
-  </div>
-  </div>
-</div>
-<Footer />
-</div>
   );
 };
 
