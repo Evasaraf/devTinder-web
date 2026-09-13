1afb6a945bb782fbf3b23bade292b6a2c71ed30c
Evasaraf <164766438+Evasaraf@users.noreply.github.com>
2026-09-14 01:16:10 +0530

    using dispatch and navigate to send the user to login page after logout

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,11 +1,19 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { removeUser } from "./utils/userslice";
+import axios from "axios";
+import { BASE_URL } from "./utils/constants";
 
 const NavBar = () => {
   const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleLogout = async() => {
     try{
      const res = await axios.post(BASE_URL + "/logout", {}, { withCredentials: true });
+     dispatch(removeUser());
+     return navigate("/login");
     }
     catch(err){
     // error logic maybe redirected to error page
