078bf137587528f267589007e57fc726d129e224
Evasaraf <164766438+Evasaraf@users.noreply.github.com>
2026-09-23 20:36:08 +0530

    if user is already login no need to fetc api again and again for every task

--- src/Body.jsx
+++ src/Body.jsx
@@ -6,11 +6,16 @@ import axios from "axios";
 import { BASE_URL } from "./utils/constants";
 import { addUser } from "./utils/userslice";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Body = () => {
   const dispatch = useDispatch();
   const Navigate=useNavigate();
+  const userData = useSelector((store) => store.user);
+
+
   const fetchUser = async () => {
+    if(userData) return; // If user data is already present, skip fetching
     try {
       const user = await axios.get(BASE_URL + "/profile/view", { withCredentials: true });
       dispatch(addUser(user.data));
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { removeUser } from "./utils/userslice";
 import axios from "axios";
